2874e907123fb646bab3e387ef69ce7f8dd8c4f8
yestobetter <161953845+yestobetter@users.noreply.github.com>
2024-03-03 12:26:36 -0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,26 @@
 // router.js
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/setting',
-        component: () => import(/* webpackChunkName: "about" */ '../views/Setting.vue')
-    },
-]
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/setting',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Setting.vue')
+  },
+];
 
 const router = new VueRouter({
-    base: '/', // Adjust base to match publicPath
-    routes
-})
+  mode: 'history', // This enables history mode
+  base: '/', // Adjust the base to match your deployment environment
+  routes
+});
 
-export default router
+export default router;
