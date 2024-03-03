14ebe93d8dbadbdb228632052ed46eef1431592b
yestobetter <161953845+yestobetter@users.noreply.github.com>
2024-03-03 11:25:47 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: false,
-  publicPath: '/Interview-Copilot-main/' // Set publicPath to repository name
+  publicPath: '/' // Set publicPath to repository name
 })
