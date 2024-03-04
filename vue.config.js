7f69536f66970886837e901a783acb921da3ebe5
yestobetter <161953845+yestobetter@users.noreply.github.com>
2024-03-03 18:58:35 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
-  transpileDependencies: false,
-  publicPath: '/' // Set publicPath to repository name
+  transpileDependencies: true,
+  publicPath: '' // 
 })
